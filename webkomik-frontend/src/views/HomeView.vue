9fed3e0799bf7b9dc773e6ae608b427935a90504
Tubagus Aldi <tubagusaldi28@gmail.com>
2025-05-18 19:02:21 +0700

    Menambahkan Tampiln HomeView sementara

---
+++ webkomik-frontend/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="text-center">
+    <h1 class="text-2xl font-bold">Halaman Beranda</h1>
+    <p>Selamat datang di Aplikasi WebKomik!</p>
+    <nav class="mt-4 space-x-2">
+      <RouterLink to="/login" class="text-blue-500 hover:underline">Login</RouterLink>
+      <RouterLink to="/register" class="text-blue-500 hover:underline">Register</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
